01e25c28d544da56c14929cd10e22bf3ceffcc11
Leah Irwin <irwin.leah@outlook.com>
2025-03-11 16:28:53 -0400

    horse-index and logo added

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Horse:
+    def __init__(self, name, breed, description, age):
+       self.name = name
+       self.breed = breed
+       self.description = description
+       self.age = age
+
+
+horses = [
+    Horse('Star', 'Thoroughbred', 'Sweet.', 19),
+    Horse('Fancy', 'Pony', 'Kinda rude.', 8 ),
+    Horse('Beau', 'Appaloosa', 'charmer.', 8),
+    Horse('Bandit', 'Quarter Horse', 'grumpy', 25),
+    Horse('Holly', 'Hanovarian', 'Chill', 14),
+    Horse('Poppy', 'Warmblood Cross', 'Big movement', 9),
+]
+
 def home(request):
     return HttpResponse('<h1>Hello 𓃗</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+
+def horse_index(request):
+    return render(request, 'horses/index.html', {'horses': horses})
