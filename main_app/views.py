34097032ab6dd5dff23380f44b89eee4ca331642
Leah Irwin <irwin.leah@outlook.com>
2025-03-11 11:33:34 -0400

    newly rendered about.html template

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Hello 𓃗</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About Horse Staff</h1>')
+    return render(request, 'about.html')
