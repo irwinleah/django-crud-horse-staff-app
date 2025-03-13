fa26bc63d3961c4075df15a25518f60e1b8c4527
Leah Irwin <irwin.leah@outlook.com>
2025-03-13 10:03:59 -0400

    edit and delete for training model works

--- main_app/models.py
+++ main_app/models.py
@@ -49,11 +49,11 @@ class Feeding(models.Model):
 
 
 class Training(models.Model):
-    locaton = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     discipline = models.CharField(max_length=100)
     
     def __str__(self):
-        return f"{self.discipline} at {self.locaton}"
+        return f"{self.discipline} at {self.location}"
     
     def get_absolute_url(self):
         return reverse('training-detail', kwargs={'pk': self.id})
